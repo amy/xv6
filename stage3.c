29c8394ade64aecdd8f50253b2e1ca1f73f8a274
Amy Chen <achen@dattobackup.com>
2016-03-27 18:53:18 -0400

    stage 3

--- stage3.c
+++ stage3.c
@@ -3,15 +3,44 @@
 #include "user.h"
 #include "signal.h"
 
+// Reference: https://piazza.com/class/ijkf1amk1f44hp?cid=166
+
+void dummy(void)
+{
+	printf(1, "TEST FAILED: this should never execute.\n");
+}
+
 void handle_signal(siginfo_t info)
 {
+	// this handler executes repeatedly
+	static int tick;
+	tick++;
+
+	// when the counter hits a ceiling, 
+	// edit the return address of the handler 
+	// to skip the x/y instruction
+	if (tick == 5000) {
+
+		uint info_address = &info;
+
+	}
 
 }
 
 int main(int argc, char *argv[])
 {
+
+	int x = 5;
+	int y = 0;
+
+	int a = uptime();
+
 	signal(SIGFPE, handle_signal);
-	
+
+	x = x / y;
+
+	int b = uptime();
+
 	printf(1, "Traps Performed: XXXX\n");
 	printf(1, "Total Elapsed Time: XXXX\n");
 	printf(1, "Average Time Per Trap: XXXXX\n");
