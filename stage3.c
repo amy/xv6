e576efd376a7d65231119b5abc3e3cae55d006ac
Amy Chen <achen@dattobackup.com>
2016-03-27 20:06:45 -0400

    stage 3 completed

--- stage3.c
+++ stage3.c
@@ -19,11 +19,11 @@ void handle_signal(siginfo_t info)
 	// when the counter hits a ceiling, 
 	// edit the return address of the handler (add 4) 
 	// to skip the x/y instruction (dereference and add 4)
-	if (trapNum == 10) {
+	if (trapNum == 1000000) {
 
-		siginfo_t * info_address = &info;
-		info_address = &info + 4;
-		info_address += 4;
+		//siginfo_t * info_address = &(info.signum);
+		int * info_address = &(info.signum) + 4;
+		(*info_address) += 4;
 
 	}
 
@@ -43,9 +43,9 @@ int main(int argc, char *argv[])
 
 	int b = uptime();
 
-	printf(1, "Traps Performed: 10\n");
-	printf(1, "Total Elapsed Time: %d\n", (b-a));
-	printf(1, "Average Time Per Trap: %d\n", (b-a)/10);
+	printf(1, "Traps Performed: 1000000\n");
+	printf(1, "Total Elapsed Time: %d\n", (b-a)); // How many ticks it takes to run 100,000 traps
+	printf(1, "Average Time Per Trap: %d\n", (b-a)); // in microticks, microtick = 1/1millionth of a tick
 
 	exit();
 }
